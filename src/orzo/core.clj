f13df28ece94cf1b086e0afab4f20648f381746d
Tiago Luchini <luchini@work.co>
2018-12-13 13:29:02 -0500

    Improve function structure

--- src/orzo/core.clj
+++ src/orzo/core.clj
@@ -6,6 +6,8 @@
                       LocalDate)
            (java.time.temporal ChronoUnit)))
 
+;; Utilities
+
 (defn ^:private parse-number
   [n]
   (try
@@ -19,6 +21,62 @@
     {:major (parse-number (nth matches 1))
      :minor (parse-number (nth matches 2))
      :patch (parse-number (nth matches 3))}))
+
+(defn ^:private greater-indicator
+  [from-indicator]
+  (case from-indicator
+    :major nil
+    :minor :major
+    :patch :minor
+    nil))
+
+(defn ^:private smaller-indicator
+  [from-indicator]
+  (case from-indicator
+    :major :minor
+    :minor :patch
+    :patch nil
+    nil))
+
+(defn ^:private simple-semver-set
+  [version {:keys [major minor patch] :as parsed-semver}]
+  (let [replace-str (str "$1" (or major 0) "." (or minor 0) "." (or patch 0) "$5")]
+    (string/replace-first version
+                          #"(\D*)(?:(\d+)\.?)?(?:(\d+)\.?)?(\d+)?(.*)"
+                          replace-str)))
+
+;; Utilities for sprint-based
+
+(defn ^:private sprint-parse-line
+  [line]
+  (let [split (string/split line #" ")]
+    {:sprint (Integer/parseInt (first split))
+     :end-date (LocalDate/parse (last split))}))
+
+(defn ^:private get-sprint-date-pair
+  [path]
+  (let [lines (some-> path io/file slurp string/split-lines)
+        last-sprint (sprint-parse-line (last lines))]
+    {:sprint (:sprint last-sprint)
+     :end-date (:end-date last-sprint)}))
+
+(defn ^:private build-sprint-calendar-semver
+  [orig-semver indicator sprint-number]
+  (let [at-indicator-value (get orig-semver indicator)
+        smaller-indicator' (smaller-indicator indicator)
+        smaller-indicator'' (smaller-indicator smaller-indicator')
+        smaller-indicator-value (get orig-semver smaller-indicator')]
+    (cond-> orig-semver
+      (not= at-indicator-value sprint-number)
+      (assoc smaller-indicator' 0)
+
+      (and smaller-indicator'' (not= at-indicator-value sprint-number))
+      (assoc smaller-indicator'' 0)
+      
+      (= at-indicator-value sprint-number)
+      (assoc smaller-indicator' (inc smaller-indicator-value))
+      
+      :always (assoc indicator sprint-number))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Seed functions
@@ -40,19 +98,16 @@
        (second matches)
        (throw (ex-info (str "regex " (.toString regex) " didn't yield results") {}))))))
 
-(defn instant
-  []
-  (.toString (Instant/now)))
-
 (defn git-last-tag
   ([]
    (git-last-tag nil))
   ([match-pattern]
    (let [base-cmd ["git" "describe" "--tags" "--abbrev=0"]
          cmd (if match-pattern (conj base-cmd (str "--match=" match-pattern)) base-cmd)
-         {:keys [exit err out]} (shell/sh cmd)]
+         {:keys [exit err out]} (apply shell/sh cmd)]
      (when (not= 0 exit)
-       (throw (ex-info "git-last-tag failed" {:reason err})))
+       (throw (ex-info "git-last-tag failed" {:reason err
+                                              :cmd cmd})))
      out)))
 
 (defn unstage
@@ -61,8 +116,55 @@
     (io/delete-file ".orzo-stage")
     version))
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Bump functions
+;; Meta functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn instant
+  []
+  (.toString (Instant/now)))
+
+(defn env
+  [var-name]
+  (System/getenv var-name))
+
+(defn git-sha
+  ([]
+   (git-sha 7))
+  ([length]
+   (let [{:keys [exit err out]} (shell/sh "git" "rev-parse" (str "--short=" length) "HEAD")]
+     (when (not= 0 exit)
+       (throw (ex-info "git-sha failed" {:reason err})))
+     (string/trim out))))
+
+(defn git-count-since-last-tag
+  ([]
+   (git-count-since-last-tag nil))
+  ([match-pattern]
+   (let [last-tag (git-last-tag match-pattern)
+         sha (git-sha)
+         cmd ["git" "rev-list" "--count" (str last-tag ".." sha)]
+         {:keys [exit err out]} (shell/sh cmd)]
+     (when (not= 0 exit)
+       (throw (ex-info "git-count-since-last-tag failed" {:reason err})))
+     out)))
+
+(defn calendar-sprint-number
+  [{:keys [sprint-size sprint-file-path]
+    :or {sprint-size 2}}]
+  (if-not sprint-file-path
+    (throw (ex-info "sprint-file-path must be provided" {})))
+  (let [{:keys [sprint end-date]} (get-sprint-date-pair sprint-file-path)
+        now (LocalDate/now)
+        weeks (-> ChronoUnit/WEEKS (.between end-date now))]
+    (if (.isAfter now end-date)
+      (+ (int (Math/ceil (/ weeks sprint-size)))
+         sprint)
+      sprint)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Transformer functions
 ;;
 ;; first parameter is always the transformed
 ;; version. The other parameters configure
@@ -70,19 +172,13 @@
 ;; returns bumped up version
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn add-string
-  ([version string-to-add]
-   (add-string version string-to-add :suffix))
-  ([version string-to-add placement]
-   (case placement
-     :suffix (str version string-to-add)
-     :prefix (str string-to-add version))))
+(defn append
+  [version value]
+  (str version value))
 
-(defn add-env
-  ([version var-name]
-   (add-env version var-name :suffix))
-  ([version var-name placement]
-   (add-string version (System/getenv var-name) placement)))
+(defn prepend
+  [version value]
+  (str value version))
 
 (defn extract-base-semver
   [version]
@@ -91,43 +187,44 @@
          (or minor 0) "."
          (or patch 0))))
 
+(defn set-semver
+  [version {:keys [major minor patch ripple?] :as opts}]
+  (if-not ripple?
+    (simple-semver-set version {:major major :minor minor :patch patch})
+    (let [semver (parse-semver version)
+          new-semver (cond-> {}
+                       (not= major (:major semver)) (assoc :major major
+                                                           :minor 0
+                                                           :patch 0)
+                       (not= minor (:minor semver)) (assoc :major major
+                                                           :minor minor
+                                                           :patch 0)
+                       (not= patch (:patch semver)) (assoc :major major
+                                                           :minor minor
+                                                           :patch patch))]
+      (simple-semver-set version new-semver))))
+
 (defn bump-semver
   [version indicator]
   (let [{:keys [major minor patch] :as semver} (parse-semver version)
-        replace-str
-        (case indicator
-          :major (str "$1" (if major (inc major) 0) "." (or minor 0) "." (or patch 0) "$5")
-          :minor (str "$1" (or major 0) "." (if minor (inc minor) 0) "." (or patch 0) "$5")
-          :patch (str "$1" (or major 0) "." (or minor 0) "." (if patch (inc patch) 0) "$5"))]
-    (string/replace-first version
-                          #"(\D*)(?:(\d+)\.?)?(?:(\d+)\.?)?(\d+)?(.*)"
-                          replace-str)))
+        major' (if (= :major indicator) (if major (inc major) 0) major)
+        minor' (if (= :minor indicator) (if minor (inc minor) 0) minor)
+        patch' (if (= :patch indicator) (if patch (inc patch) 0) patch)]
+    (set-semver version {:major major' :minor minor' :patch patch' :ripple? true})))
 
-
-(defn ^:private get-sprint-date-pair
-  [path]
-  (let [matches (some-> path io/file slurp string/split-lines last (string/split #" "))
-        sprint (first matches)
-        end-date (last matches)]
-    {:sprint (Integer/parseInt sprint)
-     :end-date (LocalDate/parse end-date)}))
-
-;; TODO: finish this
-(defn sprint-calendar-based
-  [version {:keys [start-date indicator sprint-size sprint-file-path]
-            :or {indicator :minor sprint-size 2}}]
-  (if-not sprint-file-path
-    (throw (ex-info "sprint-file-path must be provided" {})))
-  (let [{:keys [sprint end-date]} (get-sprint-date-pair sprint-file-path)
-        now (LocalDate/now)
-        weeks (-> ChronoUnit/WEEKS (.between end-date now))
-        sprints (+ (inc (int (/ weeks sprint-size)))
-                   sprint)]
-    sprints))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Validation functions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn assert-git-clean?
+  [version]
+  (let [{:keys [exit err out]} (shell/sh "git" "status" "-s")]
+    (when (not= 0 exit)
+      (throw (ex-info "assert-git-clean? failed" {:reason err})))
+    (when (not (empty? out))
+      (throw (ex-info "git repo is not clean" {:reason out})))
+    version))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -170,8 +267,12 @@
 
 (comment
   (-> (read-file (io/resource "base-version.txt"))
-      (add-string "")
+      (extract-base-semver)
+      (append "-SNAPSHOT")
+      (append (str "-g" (git-sha)))
+      (prepend "v")
       (stage))
+
 
   (-> (unstage)
       #_(git-tag)
@@ -182,8 +283,11 @@
              #"(?s)^.*\"version\"\s*:\s*\"([^\"]*).*$")
 
 
-  (sprint-calendar-based "3.4.5" {:sprint-file-path (io/resource "sprint-file")})
+  (sprint-calendar-based "3.4.5" {:indicator :minor
+                                  :sprint-file-path (io/resource "sprint-file")})
 
+
+  (calendar-sprint-number {:sprint-file-path (io/resource "sprint-file")})
 
   (get-sprint-date-pair (io/resource "sprint-file"))
   
